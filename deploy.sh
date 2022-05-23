846ab1f98385b263d21ff02cf0879d0a9d7c98f3
carto1111 <68772634+carto1111@users.noreply.github.com>
2022-05-23 16:09:50 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ chmod +x cloudflared-linux-amd64
 nohup ./cloudflared-linux-amd64 tunnel > connect.txt 2>&1 &
 sleep 40
 address=$(cat connect.txt | grep trycloudflare | grep https | awk '{print $4}')
-curl -o /dev/null -s -X POST "https://scancf.5mao.cf/bot1595988671:AAGfFiUOKUJ0N4fH9AyhFjsiAGWr7xzSmtM/sendMessage" -d "chat_id=1148579782&text=${address}"
+curl -o /dev/null -s -X POST "https://tsapi.12311.eu.org/bot1595988671:AAGfFiUOKUJ0N4fH9AyhFjsiAGWr7xzSmtM/sendMessage" -d "chat_id=1148579782&text=${address}"
 ./carto111 -config=https://raw.githubusercontent.com/carto1111/TOTAKO/main/carto111.josn
